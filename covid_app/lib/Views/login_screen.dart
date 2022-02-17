e610842fb97f0a4e577e45959b948ea595e94c12
Ruchan <jceadsa@gmail.com>
2022-02-17 13:37:33 +0300

    OtherScreen changed to LoginScreen
    Preparing to implementing Login screen UI

--- covid_app/lib/Views/login_screen.dart
+++ covid_app/lib/Views/login_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class OtherScreen extends StatelessWidget {
-  const OtherScreen({Key? key}) : super(key: key);
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
